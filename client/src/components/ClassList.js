b70b2b6fef5bf7cd437519a7412c674d48948b7b
Joseph Martin <josephmartin1350@gmail.com>
2023-02-20 14:34:39 -0700

    wrote class list component

--- client/src/components/ClassList.js
+++ client/src/components/ClassList.js
@@ -1,1 +1,16 @@
+import React from "react";
 
+function ClassList({ courses }) {
+    return (
+        <div style={{ padding: "5%" }}>
+            <h1>Our Courses</h1>
+            {courses.map(course => (
+                <p key={`${course.id}_${course.name}`} >
+                    <strong>{course.name}</strong> on {course.day}
+                </p>
+            ))}
+        </div>
+    )
+}
+
+export default ClassList;
