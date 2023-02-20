5dfc55004946829241c0f1d9f5ce46db649a02dc
Joseph Martin <josephmartin1350@gmail.com>
2023-02-20 13:56:42 -0700

    wrote application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,1 +1,25 @@
+class ApplicationController < ActionController::API
+  include ActionController::Cookies
 
+  before_action :authorize
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  private
+
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def render_not_found_response
+    render json: { error: "Item not found" }, status: :not_found
+  end
+
+  def authorize
+    @current_user = Student.find(session[:user_id])
+
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+  end
+
+end
