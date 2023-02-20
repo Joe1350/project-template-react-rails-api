526a8ac38a6fbce2e7fe0d108d9ee7ad6e9effc4
Joseph Martin <josephmartin1350@gmail.com>
2023-02-20 13:51:58 -0700

    wrote student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,1 +1,12 @@
+class Student < ApplicationRecord
+    has_many :schedules
+    has_many :courses, through: :schedules
 
+    has_secure_password
+
+    validates :name, presence: true
+    validates :age, length: { in: 18..150,
+        message: "must be over 18" }
+    validates :username, uniqueness: true
+
+end
