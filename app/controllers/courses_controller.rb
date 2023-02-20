64a5acf45519e56fd9a8253d68a1e9a1cb99c608
Joseph Martin <josephmartin1350@gmail.com>
2023-02-20 14:05:51 -0700

    wrote courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,1 +1,29 @@
+class CoursesController < ApplicationController
+    skip_before_action :authorize
 
+    def index
+        courses = Course.all.order(day: :asc)
+        render json: courses
+    end
+
+    def create
+        course = Course.create(course_params)
+        render json: course, status: :created
+    end
+
+    private
+
+    def find_course
+        Course.find(params[:id])
+    end
+
+    def course_params
+        params.permit(:id, :name, :day)
+    end
+
+end
+
+    # def show
+    #     course = find_course
+    #     render json: course
+    # end
