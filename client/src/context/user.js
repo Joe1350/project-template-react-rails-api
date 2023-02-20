955e9b2d13bf9fec8517d3f439294aec094d0d0a
Joseph Martin <josephmartin1350@gmail.com>
2023-02-20 14:30:11 -0700

    wrote user context

--- client/src/context/user.js
+++ client/src/context/user.js
@@ -1,1 +1,23 @@
+import React, { useState, useEffect } from "react";
 
+const UserContext = React.createContext()
+
+function UserProvider({ children }) {
+    const [student, setStudent] = useState(null)
+
+    useEffect(() => {
+        fetch("/me").then((r) => {
+            if (r.ok) {
+                r.json().then(user => setStudent(user));
+            }
+        });
+    }, []);
+
+    return (
+        <UserContext.Provider value={{ student, setStudent }}>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export { UserContext, UserProvider };
