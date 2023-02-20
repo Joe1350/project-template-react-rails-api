1a2a617b43553ecf738c9d81b6cacb7b4044144a
Joseph Martin <josephmartin1350@gmail.com>
2023-02-20 14:02:54 -0700

    wrote students controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,1 +1,26 @@
+class StudentsController < ApplicationController
+    skip_before_action :authorize, only: [:create, :index]
 
+    # this is here for testing
+    # def index
+    #     students = Student.all
+    #     render json: students
+    # end
+
+    def show
+        render json: @current_user
+    end
+
+    def create
+        student = Student.create!(student_params)
+        session[:user_id] = student.id
+        render json: student, status: :created
+    end
+
+    private
+
+    def student_params
+        params.permit(:id, :name, :age, :username, :password, :password_confirmation)
+    end
+
+end
