041492e817e6a727db9151cded58b88eddde318f
Joseph Martin <josephmartin1350@gmail.com>
2023-02-20 14:04:17 -0700

    wrote schedules controller

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,1 +1,37 @@
+class SchedulesController < ApplicationController
+    # wrap_parameters format: []
 
+    def index
+        schedules = @current_user.schedules
+        render json: schedules
+    end
+
+    def show
+        schedules = @current_user.schedules.find(params[:id])
+        render json: schedules
+    end
+
+    def create
+        schedule = @current_user.schedules.create!(schedule_params)
+        render json: schedule, status: :created
+    end
+
+    def update
+        schedule = @current_user.schedules.find(params[:id])
+        schedule.update(schedule_params)
+        render json: schedule
+    end
+
+    def destroy
+        schedule = @current_user.schedules.find(params[:id])
+        schedule.destroy
+        head :no_content
+    end
+
+    private
+
+    def schedule_params
+        params.permit(:id, :bring_own_supplies, :course_id)
+    end
+
+end
