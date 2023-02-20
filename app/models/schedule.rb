e3fd909c2c817a83166f997d451677587151dd76
Joseph Martin <josephmartin1350@gmail.com>
2023-02-20 13:53:09 -0700

    wrote schedules model

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,1 +1,17 @@
+class Schedule < ApplicationRecord
+    belongs_to :student
+    belongs_to :course
 
+    validate :one_class_per_day
+
+    def one_class_per_day
+        course = Course.find(self.course_id)
+        student = Student.find(self.student_id)
+        student.courses.each do |c|
+            unless c.day != course.day
+                errors.add(:schedules, "Only one class per day")
+            end
+        end
+    end
+
+end
