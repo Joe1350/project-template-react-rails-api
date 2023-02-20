217aff6d5d7bd79a49e9a9312c1c59a903f82121
Joseph Martin <josephmartin1350@gmail.com>
2023-02-20 13:55:49 -0700

    wrote course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,1 +1,9 @@
+class Course < ApplicationRecord
+    has_many :schedules, dependent: :destroy
+    has_many :students, through: :schedules
 
+    validates :name, presence: true
+    validates :day, inclusion: { in: %w(monday tuesday wednesday thursday friday),
+        message: "%{value} is not a valid day"
+    }
+end
